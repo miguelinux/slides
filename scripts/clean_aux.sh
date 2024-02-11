89d5cfe8b0e49c058164a24d6f9d712942c4229d
Miguel Bernal Marin <miguel.bernal.marin@gmail.com>
2024-02-11 00:00:57 -0600

    add clean aux

---
+++ scripts/clean_aux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+find -name \*.aux -delete
+find -name \*.nav -delete
+find -name \*.vrb -delete
+find -name \*.log -delete
+find -name \*.out -delete
+find -name \*.snm -delete
+find -name \*.toc -delete
